08708a03da2d2fd336a5616182db414761ec37b9
newbeeyhy <2983075284@qq.com>
2024-09-19 20:03:07 +0800

    finish sub

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -1,4 +1,15 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `sub' instructions here.
-*/
+
+static void instr_execute_2op() {
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_dest.val = alu_sub(opr_src.val, opr_dest.val, opr_src.data_size);
+    operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(sub, r, rm, b)
+make_instr_impl_2op(sub, r, rm, v)
+make_instr_impl_2op(sub, rm, r, b)
+make_instr_impl_2op(sub, rm, r, v)
+make_instr_impl_2op(sub, i, a, b)
+make_instr_impl_2op(sub, i, a, v)
