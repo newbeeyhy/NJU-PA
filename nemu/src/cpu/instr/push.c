6698f7de0f7f9d97dc138624c5436571183c7892
newbeeyhy <2983075284@qq.com>
2024-09-27 10:25:18 +0800

    finish push modify call

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,4 +1,9 @@
 #include "cpu/instr.h"
-/*
-Put the implementations of `push' instructions here.
-*/
+
+instr_execute_1op() {
+    uint32_t rel = paddr_read(eip + 1, data_size / 8);
+    cpu.esp -= 4;
+    paddr_write(cpu.esp, data_size / 8, rel);
+}
+
+make_instr_impl_1op(push, r, l)
