637a742de04b2071a3f4ed4a031541f7453af4e3
newbeeyhy <2983075284@qq.com>
2024-09-27 10:56:57 +0800

    modify push

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,9 +1,9 @@
 #include "cpu/instr.h"
 
 instr_execute_1op() {
-    uint32_t rel = paddr_read(eip + 1, data_size / 8);
-    cpu.esp -= 4;
-    paddr_write(cpu.esp, data_size / 8, rel);
+    operand_read(&opr_src);
+    cpu.esp -= data_size / 8;
+    paddr_write(cpu.esp, data_size / 8, opr_src.val);
 }
 
 make_instr_impl_1op(push, r, l)
