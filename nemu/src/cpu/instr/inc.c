2407805579f4f18a9f1e1655fee31aa0c122eb18
newbeeyhy <2983075284@qq.com>
2024-10-08 17:19:55 +0800

    debug op1

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -1,9 +1,11 @@
 #include "cpu/instr.h"
 
 static void instr_execute_1op() {
-    operand_read(&opr_dest);
-    opr_dest.val = alu_add(1, opr_dest.val, data_size);
-    operand_write(&opr_dest);
+    operand_read(&opr_src);
+    opr_src.val = alu_add(sign_ext((uint32_t)1, opr_src.data_size),
+                           sign_ext(opr_src.val, opr_src.data_size),
+                           data_size);
+    operand_write(&opr_src);
 }
 
 make_instr_impl_1op(inc, r, v)
