5cbd5f7db2efba106bee591bee945f125bf7a62d
newbeeyhy <2983075284@qq.com>
2024-12-27 13:54:28 +0800

    finish 4-2

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -7,35 +7,19 @@
 #define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) // number of page tables to cover the vmem
 
 PDE *get_updir();
-// PTE vptable[NR_PT * NR_PTE] align_to_page;
+PTE vptable[NR_PT * NR_PTE] align_to_page;
 
 void create_video_mapping()
 {	
-/*
 	PDE *updir = get_updir();
+	PTE *ptable = vptable;
 	for (uint32_t dir = 0; dir < NR_PT; dir++) {
+		updir[(VMEM_ADDR >> 22) + dir].val = make_pde((PTE *)va_to_pa(ptable));
 		for (uint32_t page = 0; page < NR_PTE; page++) {
-			PTE *uptable = (PTE *)((updir[(VMEM_ADDR >> 22) + dir].page_frame << 12) | (page << 2));
-			uptable->val = make_pte(VMEM_ADDR | (page << 12));
+			ptable->val = make_pte(((VMEM_ADDR >> 22) << 22) + (page << 12));
+			ptable++;
 		}
 	}
-*/
-/*
-	PDE *pdir = (PDE *)va_to_pa(get_updir());
-	PTE *ptable = (PTE *)va_to_pa(table);
-    uint32_t pdir_idx, ptable_idx, pframe_idx;
-	//fill PDE and PTEs 
-    pdir_idx = 0x0;
-	pdir[pdir_idx].val = make_pde(ptable);
-	//pdir[pdir_idx + KOFFSET / PT_SIZE].val = make_pde(ptable);
-    ptable += 0x0;
-	pframe_idx = 0x0;
-	for (ptable_idx = 0x0; ptable_idx < 1024; ptable_idx++){
-	    ptable->val = make_pte(pframe_idx << 12);
-		pframe_idx++;
-		ptable++;
-	}
-*/
 }
 
 void video_mapping_write_test()
--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,11 +5,6 @@
 paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
 #ifdef IA32_PAGE
-#ifdef HAS_DEVICE_VGA
-	if (laddr >= 0xa0000 && laddr < 0xa0000 + 320 * 200) {
-		return laddr;
-	}
-#endif
 	uint32_t dir = (laddr >> 22) & 0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
@@ -20,6 +15,11 @@ paddr_t page_translate(laddr_t laddr) {
 	ptable.val = paddr_read((pdir.page_frame << 12) | (page << 2), 4);
 	assert(ptable.present);
 	uint32_t paddr = (ptable.page_frame << 12) | offset;
+#ifdef HAS_DEVICE_VGA
+	if (laddr >= 0xa0000 && laddr < 0xa0000 + 320 * 200) {
+		assert(paddr == laddr);
+	}
+#endif
 	return paddr;
 #else
 	assert(0);
